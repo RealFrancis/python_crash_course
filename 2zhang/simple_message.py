4efdfb2b51a26b00276446a9a2efcad331873c6e
李维 <6513705+takeheart7up@user.noreply.gitee.com>
2022-06-03 19:45:56 +0800

    first commit

---
+++ 2zhang/simple_message.py
@@ -0,0 +1,14 @@
+message = 'hello world!'
+print(message)
+print(message.title())
+
+frist_name = 'wei'
+last_name = 'li'
+print(f"{last_name} {frist_name}")
+
+favorite_language = ' python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+
+print("Albert Einstein once said, \"A person who nevermade a mistake never tried anything new.\"")
